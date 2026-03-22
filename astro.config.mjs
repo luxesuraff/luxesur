f88f9053ca0f1c142a82c2f11a52a9fdc712badc
Steve Rogers <your@email.com>
2026-03-22 00:27:48 -0400

    fix astro config + enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   site: "https://www.luxesur.com",
+  output: "server",
+  adapter: vercel()
 });
